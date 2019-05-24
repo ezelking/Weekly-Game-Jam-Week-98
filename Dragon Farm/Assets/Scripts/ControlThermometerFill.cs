4671f96a28320813400a1e235332743e32c50317
ezelking <timroberson@live.nl>
2019-05-24 23:23:26 +0200

    Thermometer added
    - Decreases over Time
    - Function to increase temperature
    - UI element created

---
+++ Dragon Farm/Assets/Scripts/ControlThermometerFill.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ControlThermometerFill : MonoBehaviour
+{
+    Image fillImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fillImage = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fillImage.fillAmount = Thermometer.Instance.getTemp();
+    }
+}
---
+++ Dragon Farm/Assets/Scripts/Thermometer.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public sealed class Thermometer
+{
+    private const float minTemp = 0, maxTemp = 100;
+
+    private float tempurature = maxTemp;
+    private static Thermometer instance = null;
+    private static readonly object padlock = new object();
+
+    Thermometer()
+    {
+    }
+
+    public static Thermometer Instance
+    {
+        get
+        {
+            lock (padlock)
+            {
+                if (instance == null)
+                {
+                    instance = new Thermometer();
+                }
+                return instance;
+            }
+        }
+    }
+
+    public void IncreaseTemperature(float amount)
+    {
+        tempurature += amount;
+        if (tempurature > maxTemp)
+        {
+            tempurature = maxTemp;
+        }
+    }
+
+    public float getTemp()
+    {
+        instance--;
+        return (tempurature - minTemp) / (maxTemp - minTemp);
+    }
+
+    public static Thermometer operator --(Thermometer a)
+    {
+        a.tempurature -= Time.deltaTime;
+        if (a.tempurature > maxTemp)
+        {
+            //IMPLEMENT LOSE CONDITION
+        }
+        return a;
+    }
+}
