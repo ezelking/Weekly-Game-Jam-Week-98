eb1e5fd055d832e3e4b4d12dded10011baf093bb
ezelking <timroberson@live.nl>
2019-05-25 00:45:36 +0200

    Person classes added that allow adding of gear

---
+++ Dragon Farm/Assets/Scripts/Person.cs
@@ -0,0 +1,79 @@
+﻿public abstract class Person
+{
+    public string name;
+    public Stats originalStats;
+
+    public abstract Stats Stats();
+}
+
+public class Warrior: Person
+{
+    public Warrior(string _name, Stats _stats)
+    {
+        name = _name;
+        originalStats = _stats;
+    }
+    public override Stats Stats() {
+        return originalStats;
+    }
+}
+
+public abstract class Decorator : Person
+{
+    protected Person person;
+
+    public Decorator(Person _person)
+    {
+        person = _person;
+    }
+
+    public override Stats Stats()
+    {
+        return person.Stats();
+    }
+}
+
+class PersonwithGear : Decorator
+{
+    string gearName;
+    public Stats gearStats;
+
+    public PersonwithGear(Person _person, string _name, Stats _stats): base(_person)
+    {
+        gearName = _name;
+        gearStats = _stats;
+    }
+    public override Stats Stats()
+    {
+        return base.Stats() + gearStats;
+    }
+}
+
+public enum CharacterClasses
+{
+    Explorer,
+    Hunter,
+    Wood_Chopper,
+    Smith,
+    Cook
+}
+
+public struct Stats
+{
+    public int health { get; set; }
+    int strength { get; set; }
+    int intelligence { get; set; }
+    public Stats(int _health, int _strength, int _intelligence)
+    {
+        health = _health;
+        strength = _strength;
+        intelligence = _intelligence;
+    }
+    public static Stats operator +(Stats a, Stats b)
+    {
+        a.health += b.health;
+        a.strength += b.strength;
+        a.intelligence += b.intelligence;
+        return a;
+    }
+}
---
+++ Dragon Farm/Assets/test.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Test()
+    {
+        Person p = new Warrior("John Doe", new Stats(2,4,5));
+
+        PersonwithGear pWithGear = new PersonwithGear(p, "Strong Armor", new Stats(1, 0, 1));
+
+        Debug.Log(p.Stats().health);
+        Debug.Log(pWithGear.Stats().health);
+    }
+}
+
+[CustomEditor(typeof(test))]
+public class ObjectBuilderEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        test myScript = (test)target;
+        if (GUILayout.Button("Test Function"))
+        {
+            myScript.Test();
+        }
+    }
+}
