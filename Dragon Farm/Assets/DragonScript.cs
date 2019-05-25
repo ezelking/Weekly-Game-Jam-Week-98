ba37dc7e53dfcf9e1ba16c3995f3ae9df2077b97
ezelking <timroberson@live.nl>
2019-05-26 00:48:50 +0200

    Dragon animator changed and movement implemented

---
+++ Dragon Farm/Assets/DragonScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragonScript : MonoBehaviour
+{
+    float strength;
+    float durability;
+
+    CharacterController controller;
+    Animator animator;
+
+    public float speed;
+    float rotationSpeed;
+    float gravity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("Speed", speed);
+        controller.Move(transform.forward *speed* Time.deltaTime);
+    }
+}
