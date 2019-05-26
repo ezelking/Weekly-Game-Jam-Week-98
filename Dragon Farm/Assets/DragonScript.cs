6865d2ba6692d0b60867c592bdf3b90d6c3935f4
ezelking <timroberson@live.nl>
2019-05-26 12:44:22 +0200

    Dragon animtion added to dragon and status bar added

--- Dragon Farm/Assets/DragonScript.cs
+++ Dragon Farm/Assets/DragonScript.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DragonScript : MonoBehaviour
 {
     float strength;
-    float durability;
+    float durability = 10;
+    float rechargeTimer;
 
     CharacterController controller;
     Animator animator;
+    Image image;
 
     public float speed;
     float rotationSpeed;
@@ -17,14 +20,27 @@ public class DragonScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        rechargeTimer = 0;
+        
         controller = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
+        image = GetComponentInChildren<Image>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        rechargeTimer += Time.deltaTime;
+        image.fillAmount = rechargeTimer / durability;
+
+        if (rechargeTimer > durability)
+        {
+            animator.SetBool("BreatheFire", true);
+            rechargeTimer = 0;
+        }
+
         animator.SetFloat("Speed", speed);
         controller.Move(transform.forward *speed* Time.deltaTime);
+
     }
 }
